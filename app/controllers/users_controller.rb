039a8f15094c34ca239a6602be3d8ac77e1ac9ea
george-kirby <georgeskirby@btinternet.com>
2019-10-31 14:40:57 +0000

    added serializer to usersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
 
     def index
         users = User.all
-        render json: users
+        render json: serialize(users)
     end
 
     def show
         user = User.find(params[:id])
-        render json: user
+        render json: serialize(user)
     end
     
     def create
@@ -30,6 +30,18 @@ class UsersController < ApplicationController
     # end
 
     private
+
+    def serialize(user_data)
+        user_data.to_json(:include => {
+            :projects => {
+                :except => [:created_at, :updated_at, :id, :user_id],
+                :include => [:tasks => {
+                    :except => [:created_at, :updated_at, :id, :project_id],
+                    :include => [:steps => {:except => [:created_at, :updated_at, :id, :task_id]}]
+                }]
+            }
+        })
+    end
 
     def user_params
         params.require(:user).permit(:email, :password)
