56db9d0acdad237dba99b0ce698a689b47f3b9bd
george-kirby <georgeskirby@btinternet.com>
2019-11-01 10:16:09 +0000

    user has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+
     has_many :projects
     has_many :tasks, through: :projects
     has_many :steps, through: :tasks
 
     validates :email, presence: true
-    validates :password, presence: true
 end
