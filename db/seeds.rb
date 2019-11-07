72e8b6148cd3f3bd3f77b21bb37cd8b5076c93a3
george-kirby <georgeskirby@btinternet.com>
2019-11-07 15:46:02 +0000

    adapted seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,19 @@ User.destroy_all
 
 geo = User.create(email: "g@g.com", password: "p")
 
-get_wine = Task.create(user: geo, title: "Get wine", cue: "on commute home", display_time: "evening", actual_time: Time.new(2019, 11, 25, 18, 30), position_at_time: 1)
-choose_clothes = Task.create(user: geo, title: "Choose clothes", cue: "after dinner", display_time: "evening", actual_time: Time.new(2019, 11, 25, 18, 30), position_at_time: 2)
-write_card = Task.create(user: geo, title: "Write card", cue: "after choosing clothes", display_time: "evening", actual_time: Time.new(2019, 11, 25, 18, 30), position_at_time: 3)
-plan_travel = Task.create(user: geo, title: "Plan travel", cue: "after breakfast", display_time: "morning", actual_time: Time.new(2019, 11, 26, 10, 00), position_at_time: 1)
-
-wine_s1 = Step.create(task: get_wine, act: "go to off-licence", position: 1)
-clothes_s1 = Step.create(task: choose_clothes, act: "open wardrobe, choose from shirts to wear", position: 1)
-clothes_s2 = Step.create(task: choose_clothes, act: "set up ironing board and iron", position: 2)
-card_s1 = Step.create(task: write_card, act: "sit at desk with pen and card", position: 1)
-travel_s1 = Step.create(task: plan_travel, act: "phone, search maps for Bob's address", position: 1)
+get_wine = Task.create(user: geo, title: "Get wine", cue: "on commute home", display_time: "evening", actual_time: Time.new(2019, 11, 25, 18, 30), position_at_time: 1, 
+tags: ["bob's party", "commute"],
+steps: ["go to offy", "1 rose, 1 red, 3 white", "some rum too"]
+)
+choose_clothes = Task.create(user: geo, title: "Choose clothes", cue: "after dinner", display_time: "evening", actual_time: Time.new(2019, 11, 25, 18, 30), position_at_time: 2,
+tags: ["bob's party"],
+steps: ["open wardrobe, choose from shirts to wear", "set up ironing board and iron"]
+)
+write_card = Task.create(user: geo, title: "Write card", cue: "after choosing clothes", display_time: "evening", actual_time: Time.new(2019, 11, 25, 18, 30), position_at_time: 3,
+tags: ["bob's party"],
+steps: ["sit at desk with pen and card"]
+)
+plan_travel = Task.create(user: geo, title: "Plan travel", cue: "after breakfast", display_time: "morning", actual_time: Time.new(2019, 11, 26, 10, 00), position_at_time: 1,
+tags: ["bob's party"],
+steps: ["phone, search maps for Bob's address"]
+)
