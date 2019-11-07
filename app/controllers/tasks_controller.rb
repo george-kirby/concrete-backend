b902d0eb9d7e6321d8eb0e67aca4a7a946b9e3b0
george-kirby <georgeskirby@btinternet.com>
2019-11-07 18:26:24 +0000

    added to task_params, for tags and incomplete_steps, conditionally

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,7 +24,7 @@ class TasksController < ApplicationController
     end
 
     def update
-        # byebug
+        byebug
         task = Task.find(params[:id])
         task.update(task_params)
         if task.valid?
@@ -47,7 +47,9 @@ class TasksController < ApplicationController
 
     def task_params
         improved_task_params = params.require(:task).permit(:title, :project_id, :cue, :actual_time, :display_time, :position_at_time, :complete_steps, :incomplete_steps, :tags)
-        improved_task_params[:complete_steps] = JSON.parse(improved_task_params[:complete_steps])
+        improved_task_params[:complete_steps] = JSON.parse(improved_task_params[:complete_steps]) if improved_task_params[:complete_steps]
+        improved_task_params[:incomplete_steps] = JSON.parse(improved_task_params[:incomplete_steps]) if improved_task_params[:incomplete_steps]
+        improved_task_params[:tags] = JSON.parse(improved_task_params[:tags]) if improved_task_params[:tags]
         improved_task_params
     end
 end
