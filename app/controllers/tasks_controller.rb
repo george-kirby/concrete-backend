4c35b98e4168d492b6a0bd87cd4950202553cdf1
george-kirby <georgeskirby@btinternet.com>
2019-11-09 10:39:17 +0000

    added position_at_time incrementing to update; extracted

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,12 +11,10 @@ class TasksController < ApplicationController
     end
 
     def create
+        # byebug
         task = Task.create(task_params)
         if task.valid?
-            same_time_tasks = Task.all.filter{|t| (t.actual_time == task.actual_time) && (t != task)}.sort_by{|task| task.position_at_time}
-            if !same_time_tasks.empty?
-                task.update(position_at_time: same_time_tasks.last.position_at_time + 1)
-            end
+            last_position_at_time(task)
             render json: serialize(task)
         else
             render json: task.errors.full_messages
@@ -24,10 +22,11 @@ class TasksController < ApplicationController
     end
 
     def update
-        byebug
+        # byebug
         task = Task.find(params[:id])
         task.update(task_params)
         if task.valid?
+            last_position_at_time(task)
             render json: serialize(task)
         else
             render json: task.errors.full_messages
@@ -45,8 +44,16 @@ class TasksController < ApplicationController
         task_data.to_json(:except => [:created_at, :updated_at])
     end
 
+    def last_position_at_time(task)
+        same_time_tasks = Task.all.filter{|t| (t.actual_time == task.actual_time) && (t != task)}
+        if !same_time_tasks.empty?
+            ordered_same_time_tasks = same_time_tasks.sort_by{|task| task.position_at_time}
+            task.update(position_at_time: ordered_same_time_tasks.last.position_at_time + 1)
+        end
+    end
+
     def task_params
-        improved_task_params = params.require(:task).permit(:title, :project_id, :cue, :actual_time, :display_time, :position_at_time, :complete_steps, :incomplete_steps, :tags)
+        improved_task_params = params.require(:task).permit(:user_id, :title, :project_id, :cue, :actual_time, :display_time, :position_at_time, :complete_steps, :incomplete_steps, :tags)
         improved_task_params[:complete_steps] = JSON.parse(improved_task_params[:complete_steps]) if improved_task_params[:complete_steps]
         improved_task_params[:incomplete_steps] = JSON.parse(improved_task_params[:incomplete_steps]) if improved_task_params[:incomplete_steps]
         improved_task_params[:tags] = JSON.parse(improved_task_params[:tags]) if improved_task_params[:tags]
